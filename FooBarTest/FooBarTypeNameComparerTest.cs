e28ba5f9b6211e529123ee8d57621819aaf5e4fd
Hemmachat Paramabuddhi <hemmachat@gmail.com>
2018-09-08 22:13:14 +1000

    Add negative tests

--- FooBarTest/FooBarTypeNameComparerTest.cs
+++ FooBarTest/FooBarTypeNameComparerTest.cs
@@ -75,6 +75,25 @@ namespace FooBarTest
         }
 
         [Fact]
+        public void Match_Alternative_Second_Names_Valid()
+        {
+            var f1 = new FooBar()
+            {
+                Name = "f1",
+                Type = FizzType.Foo
+            };
+
+            var f2 = new FooBar()
+            {
+                Name = "f2",
+                Type = FizzType.Foo,
+                AlternativeNames = new List<string>() { "f1" }
+            };
+
+            Assert.True(_comparer.Equals(f1, f2));
+        }
+
+        [Fact]
         public void Match_Case_Alternative_Names_Valid()
         {
             var f1 = new FooBar()
@@ -130,14 +149,32 @@ namespace FooBarTest
         }
 
         [Fact]
-        public void Unmatch_Alternative_Names_Invalid()
+        public void Unmatch_Alternative_First_Names_Invalid()
+        {
+            var f1 = new FooBar()
+            {
+                Name = "f1",
+                Type = FizzType.Zebra,
+                AlternativeNames = new List<string>() { "ff1" }
+            };
+
+            var f2 = new FooBar()
+            {
+                Name = "f2",
+                Type = FizzType.Zebra
+            };
+
+            Assert.False(_comparer.Equals(f1, f2));
+        }
+
+        [Fact]
+        public void Unmatch_Alternative_Second_Names_Invalid()
         {
             var f1 = new FooBar()
             {
                 Name = "f1",
                 Type = FizzType.Zebra
             };
-
             var f2 = new FooBar()
             {
                 Name = "f2",
@@ -147,5 +184,102 @@ namespace FooBarTest
 
             Assert.False(_comparer.Equals(f1, f2));
         }
+
+        [Fact]
+        public void First_Empty_Invalid()
+        {
+            var f1 = new FooBar();
+
+            var f2 = new FooBar()
+            {
+                Name = "f2",
+                Type = FizzType.Zebra
+            };
+
+            Assert.False(_comparer.Equals(f1, f2));
+        }
+
+        [Fact]
+        public void First_Name_Empty_Invalid()
+        {
+            var f1 = new FooBar() 
+            {
+                Type = FizzType.Zebra
+            };
+
+            var f2 = new FooBar()
+            {
+                Name = "f2",
+                Type = FizzType.Zebra
+            };
+
+            Assert.False(_comparer.Equals(f1, f2));
+        }
+
+        [Fact]
+        public void First_Type_Empty_Invalid()
+        {
+            var f1 = new FooBar() 
+            {
+                Name = "f1"
+            };
+
+            var f2 = new FooBar()
+            {
+                Name = "f2",
+                Type = FizzType.Zebra
+            };
+
+            Assert.False(_comparer.Equals(f1, f2));
+        }
+
+        [Fact]
+        public void Second_Name_Empty_Invalid()
+        {
+            var f2 = new FooBar() 
+            {
+                Type = FizzType.Zebra
+            };
+
+            var f1 = new FooBar()
+            {
+                Name = "f2",
+                Type = FizzType.Zebra
+            };
+
+            Assert.False(_comparer.Equals(f1, f2));
+        }
+        
+        [Fact]
+        public void Second_Type_Empty_Invalid()
+        {
+            var f2 = new FooBar() 
+            {
+                Name = "f1"
+            };
+
+            var f1 = new FooBar()
+            {
+                Name = "f2",
+                Type = FizzType.Zebra
+            };
+
+            Assert.False(_comparer.Equals(f1, f2));
+        }
+
+        [Fact]
+        public void Second_Empty_Invalid()
+        {
+
+            var f1 = new FooBar()
+            {
+                Name = "f1",
+                Type = FizzType.Zebra
+            };
+
+            var f2 = new FooBar();
+
+            Assert.False(_comparer.Equals(f1, f2));
+        }        
     }
 }
